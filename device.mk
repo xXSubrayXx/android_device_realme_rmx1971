1a7a97e1fc4927381de21aea920487c044782068
Subray <61550293+XSubrayX@users.noreply.github.com>
2020-07-24 17:47:56 +0300

    Revert "RMX9171: Apadt"
    This reverts commit ec0bbc262e12ea419d3fa78960a99d1e616367c9.

--- device.mk
+++ device.mk
@@ -335,9 +335,9 @@ PRODUCT_BOOT_JARS += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.base@1.0_vendor \
+    android.hidl.base@1.0_system \
     android.hidl.manager@1.0 \
-    android.hidl.manager@1.0_vendor \
+    android.hidl.manager@1.0_system \
     android.hidl.manager@1.0-java
 
 # Power
