5d7bd2f9a1c0b22150c4de17f4188853620f4c7b
Subray <61550293+XSubrayX@users.noreply.github.com>
2020-07-29 18:32:44 +0300

    RMX1971: Proper HIDL

--- device.mk
+++ device.mk
@@ -322,6 +322,13 @@ PRODUCT_BOOT_JARS += \
     QPerformance \
     UxPerformance
 
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.base@1.0_system \
+    android.hidl.manager@1.0 \
+    android.hidl.manager@1.0_system
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.3-service.pixel-libperfmgr
