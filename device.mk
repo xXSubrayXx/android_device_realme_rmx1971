2ec8dfd142229ee26a47b8f04dd24ff7be7ac0af
Subray <61550293+XSubrayX@users.noreply.github.com>
2020-08-02 21:45:16 +0300

    RMX1971: Camera regen

--- device.mk
+++ device.mk
@@ -139,16 +139,13 @@ PRODUCT_PACKAGES += \
     BluetoothResCommon \
     libldacBT_dec
 
-# Camera
+ Camera
 PRODUCT_PACKAGES += \
-    android.frameworks.displayservice@1.0 \
-    android.hardware.camera.device@3.4 \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.5 \
     android.hardware.camera.provider@2.4-service \
-    android.hardware.camera.common@1.0 \
-    vendor.qti.hardware.camera.device@1.0 \
-    Snap
+    Snap \
+    libdng_sdk.vendor \
+    vendor.qti.hardware.camera.device@1.0.vendor
 
 # CNE
 PRODUCT_PACKAGES += \
