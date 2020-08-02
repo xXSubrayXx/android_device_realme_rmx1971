a5ca77eaad13c79afa10a1e3c9991c8e3041ca08
Subray <61550293+XSubrayX@users.noreply.github.com>
2020-08-02 21:42:25 +0300

    Revert "RMX1971: use sdk"
    This reverts commit f9f4c1311d3c825ca9d160bdf7db5d06a4bd4d48.

--- device.mk
+++ device.mk
@@ -141,7 +141,13 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    libdng_sdk.vendor \
+    android.frameworks.displayservice@1.0 \
+    android.hardware.camera.device@3.4 \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.5 \
+    android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.common@1.0 \
+    vendor.qti.hardware.camera.device@1.0 \
     Snap
 
 # CNE
