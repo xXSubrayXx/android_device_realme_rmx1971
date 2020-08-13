5c857ed2bb62c90ded6aa023f208e263815ff771
Subray <subraymusic@gmail.com>
2020-08-13 05:00:27 +0300

    RMX1971: revert media omx hal

--- device.mk
+++ device.mk
@@ -311,7 +311,12 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
-    
+ 
+PRODUCT_PACKAGES += \
+    libstagefright_omx.vendor \
+    android.hardware.media.omx@1.0-service \
+    libgui_vendor 
+
 # Net
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
